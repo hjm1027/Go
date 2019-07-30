cd479ec8f85ba8dc5b50282452399add78c02d03
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-07-30 18:50:13 +0800

    Update 3.go

--- 3.go
+++ 3.go
@@ -6,11 +6,11 @@ import ("log"
 
 func main() {
     doc, err := goquery.NewDocument("https://studygolang.com/topics")
-	if err != nil {
-	    log.Fatal(err)
-	    }
-	doc.Find(".topics .topic").Each(func(i int, contentSelection *goquery.Selection) {
-	title := contentSelection.Find(".title a").Text()
-	log.Println("第", i+1, "个帖子的标题：", title)
+    if err != nil {
+	log.Fatal(err)
+    }
+    doc.Find(".topics .topic").Each(func(i int, contentSelection *goquery.Selection) {
+    	title := contentSelection.Find(".title a").Text()
+    	log.Println("第", i+1, "个帖子的标题：", title)
 	 })
 }
