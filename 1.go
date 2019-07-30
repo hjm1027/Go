778239a2574ba76f80941c8b96bb754fa1cc362b
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-07-30 17:24:41 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,23 +1,13 @@
 package main
-
- import(
-
+import(
     "fmt"
-
     "io/ioutil"
-
     "net/http"
-
 )
 
 func main(){
-
     response,_:=http.Get("http://www.baidu.com")
-
     defer response.Body.Close()
-
     body,_:=ioutil.ReadAll(response.Body)
-
     fmt.Println(string(body))
-
 }
